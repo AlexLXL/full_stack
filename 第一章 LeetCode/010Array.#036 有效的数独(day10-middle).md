838d32b9d09c35a2d5ddaf899f9b729314fb2ee8
alexLang <l738367@tom.com>
2021-10-09 07:55:28 +0800

    feat(第一章 数据结构与算法): :rocket: 2021/10/09更新

---
+++ 第一章 LeetCode/010Array.#036 有效的数独(day10-middle).md
@@ -0,0 +1,62 @@
+
+### 题目: 有效的数独
+> 请你判断一个 9x9 的数独是否有效。只需要 根据以下规则 ，验证已经填入的数字是否有效即可。      
+> 数字 1-9 在每一行只能出现一次。  
+> 数字 1-9 在每一列只能出现一次。  
+> 数字 1-9 在每一个以粗实线分隔的 3x3 宫内只能出现一次。（请参考示例图）  
+> 数独部分空格内已填入了数字，空白格用 '.' 表示。  
+      
+![示例图](./img/036_01.jpg)  
+
+---
+思路一:  
+1.三个对象(行、列、单元格),遍历,将出现过的值放到里面  
+2.通过 let boxIndex = ~~(j/3)*3 + ~~(i/3) 获取当前单元格  
+
+思路二:   
+1.通过二位数组和三维数组存储行、列和单元格,遍历,将出现过的值放到数组对应下标  
+2.通过 let index = num.charCodeAt() - '0'.charCodeAt() - 1 获取数组下标  
+![示例图](./img/036_02.jpg)
+---
+
+```
+/**
+ * @param {character[][]} board
+ * @return {boolean}
+ * 官方解法
+ */
+var isValidSudoku = function(board) {
+    let row = Array(9).fill(0).map(() => Array(9).fill(0))
+    let colums = Array(9).fill(0).map(() => Array(9).fill(0))
+    let boxs = Array(3).fill(0).map(() => Array(3).fill(0).map(() => Array(9).fill(0)))
+    for(let j = 0; j < 9; j++) {
+        for(let i = 0; i < 9; i++) {
+            let num  = board[j][i]
+            if(num !== '.') {
+                let index = num.charCodeAt() - '0'.charCodeAt() - 1
+                row[j][index]++
+                colums[i][index]++
+                boxs[~~(j/3)][~~(i/3)][index]++
+                if(row[j][index] > 1 || colums[i][index] > 1 || boxs[~~(j/3)][~~(i/3)][index] > 1) {
+                    return false
+                }
+            }
+        }
+    }
+
+    return true
+};
+```
+
+* 技巧:  
+1.'0'.charCodeAt()  =>  48  // String转Number - 1
+
+* 优化空间: 无
+
+* 学习:  
+1.使用二位数组存储行、列出现的值  
+2.使用三维数组存储单元格出现的值
+
+* 总结: 无
+
+
--- 第一章 LeetCode/test.js
+++ 第一章 LeetCode/test.js
@@ -1,27 +1,30 @@
 /**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
- * 执行用时：68 ms, 在所有 JavaScript new Set提交中击败了92.22%的用户
- * 内存消耗：39.8 MB, 在所有 JavaScript 提交中击败了39.68%的用户
+ * @param {character[][]} board
+ * @return {boolean}
+ * 官方解法
  */
-var twoSum = function(nums, target) {
-    let set = new Set()
-    let setIdx = new Set()
-    for(let i = 0; i < nums.length; i++) {
-        let prop = nums[i]
-        if (set.has(prop)) {
-            let ids = Array.from(setIdx)[Array.from(set).indexOf(prop)]
-            if(ids !== i) {
-                return [ids, i]
+var isValidSudoku = function(board) {
+    let row = Array(9).fill(0).map(() => Array(9).fill(0))
+    let colums = Array(9).fill(0).map(() => Array(9).fill(0))
+    let boxs = Array(3).fill(0).map(() => Array(3).fill(0).map(() => Array(9).fill(0)))
+    for(let j = 0; j < 9; j++) {
+        for(let i = 0; i < 9; i++) {
+            let num  = board[j][i]
+            if(num !== '.') {
+                let index = num.charCodeAt() - '0'.charCodeAt() - 1
+                row[j][index]++
+                colums[i][index]++
+                boxs[~~(j/3)][~~(i/3)][index]++
+                if(row[j][index] > 1 || colums[i][index] > 1 || boxs[~~(j/3)][~~(i/3)][index] > 1) {
+                    return false
+                }
             }
-        }else {
-            if (!set.has(target - prop)) {
-                setIdx.add(i)
-            }
-            set.add(target - prop)
         }
     }
+
+    return true
 };
 
-console.log(twoSum([1,1,1,1,1,4,1,1,1,1,1,7,1,1,1,1,1], 11))
+
+let board = [["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]
+isValidSudoku(board)
