c836d520d15e41c87fabcd5bab79477d57ea8076
alexLang <l738367@tom.com>
2021-10-14 20:54:11 +0800

    feat(第一章 设计模式): :wrench: 2021/10/14更新

---
+++ 第一章 设计模式/005.单例模式(day19-class20).ts
@@ -0,0 +1,59 @@
+/**
+ * ES6-class 实现单例
+ */
+class Window01 {
+    private static instance: Window01
+    private constructor() {}
+    public static getInstance() {
+        if (!Window01.instance) {
+            Window01.instance = new Window01()
+        }
+        return Window01.instance
+    }
+}
+// let w1 = Window01.getInstance()
+// let w2 = Window01.getInstance()
+// console.log(w1 === w2)  // OUTPUT: true
+
+
+/**
+ * ES5-function 实现单例
+ * 缺点: 要告诉用户需使用getInstance实例化
+ */
+interface Window02 {}
+function Window02() {}
+Window02.getInstance = (function () {
+    let instance: Window02;
+    return function () {
+        if (!instance) {
+            instance = new (Window02 as any)()
+        }
+        return instance
+    }
+})()
+// let w1 = Window02.getInstance()
+// let w2 = Window02.getInstance()
+// console.log(w1 === w2)  // OUTPUT: true
+
+/**
+ * ES5-function 透明单例
+ * 偷偷给用户做单例,用户无感知
+ */
+interface Window03 {}
+let Window03 = (function () {
+    let instance: Window03
+    // ts函数默认第一个值可声明this
+    return function (this: Window03) {
+        if (!instance) {
+            instance = this
+        }
+        return instance
+    }
+})()
+// let w1 = new (Window03 as any)()
+// let w2 = new (Window03 as any)()
+// console.log(w1 === w2)  // OUTPUT: true
+
+/**
+ * 单例与构建过程分离
+ */
