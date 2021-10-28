e739c43e579a77e25eeb87960faaad9bc3dfa99b
alexLang <l738367@tom.com>
2021-10-28 17:55:51 +0800

    feat(测试): 测试代码 2021/10/28更新

--- 第九章 Test/001.test/test.js
+++ 第九章 Test/001.test/test.js
@@ -5,10 +5,18 @@ class  Animal {
             b:2
         }
     }
+    add() {
+        console.log(this)
+    }
 }
 
+// let a1 = new Animal()
+// let a2 = new Animal()
+// a1.data.a = 2
+// console.log(a1.data)
+// console.log(a2.data)
+
 let a1 = new Animal()
-let a2 = new Animal()
-a1.data.a = 2
-console.log(a1.data)
-console.log(a2.data)
+let a = a1.add
+a()
+
