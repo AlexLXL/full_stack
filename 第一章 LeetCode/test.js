1bef291c3ed39ccd7948b918bcece7b4aede4d4a
alexLang <l738367@tom.com>
2021-11-02 09:37:42 +0800

    feat(第一章 LeetCode): 2021/11/02更新

--- 第一章 LeetCode/test.js
+++ 第一章 LeetCode/test.js
@@ -1,16 +1,74 @@
-var strStr = function(haystack, needle) {
-    if(needle === "") { return 0 }
-    let hLen = haystack.length
-    let nlen = needle.length
-    for(let i = 0, j = 0; i <= hLen; i++) {
-        if(j < nlen) {
-            haystack[i] === needle[j] ? j++ : (j = 0)
-        }else {
-            return i - nlen
-        }
+var isValidBST = function (root) {
+    if (root === null) {
+        return true
     }
-    return -1
+    let result = []
+    if (root.left) {
+        if (root.val <= root.left.val) {
+            return false
+        }
+        if (root.left.left) {
+            if (root.val <= root.left.left.val) {
+                return false
+            }
+        }
+        if (root.left.right) {
+            if (root.val <= root.left.right.val) {
+                return false
+            }
+        }
+        let r1 = isValidBST(root.left)
+        result.push(r1)
+    }
+    if (root.right) {
+        if (root.val >= root.right.val) {
+            return false
+        }
+        if (root.right.left) {
+            if (root.val >= root.right.left.val) {
+                return false
+            }
+        }
+        if (root.right.right) {
+            if (root.val >= root.right.right.val) {
+                return false
+            }
+        }
+        let r2 = isValidBST(root.right)
+        result.push(r2)
+    }
+    return result.includes(false) ? false : true
 };
 
-console.log(strStr("mississippi", "issip"))
-console.log("d是的")
+function TreeNode(val, left, right) {
+    this.val = (val === undefined ? 0 : val)
+    this.left = (left === undefined ? null : left)
+    this.right = (right === undefined ? null : right)
+}
+
+let tree = new TreeNode(32,
+    new TreeNode(26,
+        new TreeNode(19,
+            undefined,
+            new TreeNode(27)
+        ),
+        undefined
+    ),
+    new TreeNode(47,
+        undefined,
+        new TreeNode(56)
+    )
+)
+
+let tree1 = new TreeNode(5,
+    new TreeNode(1,
+        undefined,
+        undefined
+    ),
+    new TreeNode(4,
+        new TreeNode(3),
+        new TreeNode(6)
+    )
+)
+
+console.log(isValidBST(tree))
