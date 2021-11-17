f4eaea5dcc468afc671b2ff4ef066f85d457520d
alexLang <l738367@tom.com>
2021-11-17 16:44:56 +0800

    feat(第五章 实战Node): 039_01.koa基础使用 2021/11/17更新

---
+++ 第五章 实战Node/039_01.koa基础使用.js
@@ -0,0 +1,31 @@
+/**
+ * koa:
+ *      - 基于promise [由express原班人马打造]
+ *      - 增强req和res [生成request和response =统一组成=> ctx]
+ *      - 有自己的中间件机制 []
+ *      - 轻巧(利用原型链写)
+ * express: 基于express
+ */
+
+/**
+ * 基础使用
+ */
+let Koa = require('koa')
+
+let app = new Koa()
+app.use((ctx) => {
+    ctx.body = 'hello-world'
+
+    // 以下方式都能访问, 但一般用最后的,前两个是原生, 后两个是封装[扩展了新属性pathname/query]
+    /*console.log(ctx.req.path)
+    console.log(ctx.request.req.path)
+    console.log(ctx.request.path)
+    console.log(ctx.path)*/
+
+})
+app.on('error', (err) => {
+    console.log(`ERROR: ${err}`)
+})
+app.listen('9090', () => {
+    console.log(`server start 9090`)
+})
