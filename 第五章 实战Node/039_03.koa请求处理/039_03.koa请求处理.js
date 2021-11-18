2e5579137f39398e6ce7c19b101c6f9b0de91d33
alexLang <l738367@tom.com>
2021-11-18 11:41:16 +0800

    feat(第五章章 实战Node): koa: 请求处理6 -- 请求文件 2021/11/18更新

--- 第五章 实战Node/039_03.koa请求处理/039_03.koa请求处理.js
+++ 第五章 实战Node/039_03.koa请求处理/039_03.koa请求处理.js
@@ -2,10 +2,13 @@ let Koa = require('koa');
 let path = require('path');
 let login = require('./page/login')
 let bodyParser = require('./middles/bodyParser')
-// let bodyParser = require('koa-bodyparser')   // koa第三方模块
+// let bodyParser = require('koa-bodyparser')   // 解析body
+let static = require('koa-static')   // 静态服务
 
 let app = new Koa();
 app.use(bodyParser({dir:path.resolve(__dirname,'upload')}))
+app.use(static(__dirname))
+app.use(static(path.resolve(__dirname, 'middles')))
 login(app)
 
 app.listen(3000, function() {
