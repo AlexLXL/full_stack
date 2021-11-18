01fcb7707b7b91adf0c3ba7dfa74b6f787f71522
alexLang <l738367@tom.com>
2021-11-18 11:47:23 +0800

    feat(第五章章 实战Node): koa: 请求处理6 -- 实现koa-static 2021/11/18更新

--- 第五章 实战Node/039_03.koa请求处理/039_03.koa请求处理.js
+++ 第五章 实战Node/039_03.koa请求处理/039_03.koa请求处理.js
@@ -2,8 +2,9 @@ let Koa = require('koa');
 let path = require('path');
 let login = require('./page/login')
 let bodyParser = require('./middles/bodyParser')
+let static = require('./middles/static')
 // let bodyParser = require('koa-bodyparser')   // 解析body
-let static = require('koa-static')   // 静态服务
+// let static = require('koa-static')   // 静态服务
 
 let app = new Koa();
 app.use(bodyParser({dir:path.resolve(__dirname,'upload')}))
