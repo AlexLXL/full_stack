39cf84a4a8389c8ce02ca46005e78663aca5f531
alexLang <l738367@tom.com>
2021-11-25 14:18:58 +0800

    feat(第六章 实现React): 7.类组件使用

--- 第六章 实现React/099.testproject/src/index.js
+++ 第六章 实现React/099.testproject/src/index.js
@@ -1,8 +1,8 @@
-import React from "./core/react";
-import ReactDOM from "./core/react-dom";
+// import React from "./core/react";
+// import ReactDOM from "./core/react-dom";
 
-// import React from "react";
-// import ReactDOM from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom";
 
 /**
  * 1.直接定义组件
@@ -26,7 +26,7 @@ ReactDOM.render(element2, document.getElementById("root"));*/
  * 2.函数组件
  * 大写字母开头
  */
-function ShopList(props) {
+/*function ShopList(props) {
     return (
         <h1 id={props.id}  className="title" style={{ color: "red" }}>
             {props.title}
@@ -34,4 +34,20 @@ function ShopList(props) {
     )
 }
 
-ReactDOM.render(<ShopList id='sl1' title='hello-world3' />, document.getElementById("root"));
+ReactDOM.render(<ShopList id='sl1' title='hello-world3' />, document.getElementById("root"));*/
+
+/**
+ * 2.类组件
+ */
+class ShopList extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return <h1 id={this.props.id}  className="title" style={{ color: "red" }}>
+            {this.props.title}
+        </h1>
+    }
+}
+
+ReactDOM.render(<ShopList id='sl1' title='hello-world4' />, document.getElementById("root"));
