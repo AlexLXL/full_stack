d31038a51d859b8688e863984ef74b9cf55d5a0b
alexLang <l738367@tom.com>
2021-11-28 20:39:09 +0800

    feat(第六章 实现React): 43.React.useContext 爷孙传递参数基础使用

--- 第六章 实现React/099.testReactHook/src/index.js
+++ 第六章 实现React/099.testReactHook/src/index.js
@@ -1,8 +1,8 @@
-import React from "./core/react";
-import ReactDOM from "./core/react-dom";
+// import React from "./core/react";
+// import ReactDOM from "./core/react-dom";
 
-// import React from 'react'
-// import ReactDOM from 'react-dom'
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 /**
  * 1. React.useState基础使用
@@ -70,7 +70,7 @@ ReactDOM.render(<App/>, document.getElementById('root'));*/
 /**
  * 3. React.useReducer
  */
-function reducer(state = {number: 0}, action) {
+/*function reducer(state = {number: 0}, action) {
     switch (action.type) {
         case 'ADD':
             return {number: state.number + 1};
@@ -92,4 +92,41 @@ function Counter() {
     )
 }
 
-ReactDOM.render(<Counter/>, document.getElementById('root'));
+ReactDOM.render(<Counter/>, document.getElementById('root'));*/
+
+
+/**
+ * 4. React.useContext
+ */
+const CounterContext = React.createContext();
+
+function reducer(state, action) {
+    switch (action.type) {
+        case 'add':
+            return {number: state.number + 1};
+        case 'minus':
+            return {number: state.number - 1};
+        default:
+            return state;
+    }
+}
+function Counter(){
+    let {state,dispatch} = React.useContext(CounterContext);
+    return (
+        <div>
+            <p>{state.number}</p>
+            <button onClick={() => dispatch({type: 'add'})}>+</button>
+            <button onClick={() => dispatch({type: 'minus'})}>-</button>
+        </div>
+    )
+}
+function App(){
+    const [state, dispatch] = React.useReducer(reducer, {number:0});
+    return (
+        <CounterContext.Provider value={{state,dispatch}}>
+            <Counter/>
+        </CounterContext.Provider>
+    )
+}
+
+ReactDOM.render(<App/>,document.getElementById('root'));
