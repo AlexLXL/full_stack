3c8ce73f4e1e558be8fc391361a8782ffa019fff
alexLang <l738367@tom.com>
2021-11-29 16:04:00 +0800

    feat(第六章 实现React): 57.NavLink基础使用, 添加选中样式

--- 第六章 实现React/100.testReactRouter/src/components/Protect.js
+++ 第六章 实现React/100.testReactRouter/src/components/Protect.js
@@ -19,4 +19,8 @@ export default Protected;
  * 指定一个route组件如何渲染有三种方式
  * 1.component  // 渲染固定组件
  * 2.render // 条件判断
+ * 1/2都是匹配路径,然后渲染组件
+ *
+ * 3.childre
+ * 3.不管路径是否匹配, 都渲染
  */
--- 第六章 实现React/100.testReactRouter/src/index.js
+++ 第六章 实现React/100.testReactRouter/src/index.js
@@ -3,7 +3,7 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {BrowserRouter as Router, Route, Switch, Redirect, Link} from './react-router-dom';
+import {BrowserRouter as Router, Route, Switch, Redirect, Link, NavLink} from 'react-router-dom';
 import Home from './components/Home';
 import User from './components/User';
 import Profile from './components/Profile';
@@ -20,9 +20,9 @@ import Protect from './components/Protect';
 ReactDOM.render(
     <Router>
         <ul>
-            <li><Link to='/'>首页</Link></li>
-            <li><Link to='/user'>用户</Link></li>
-            <li><Link to='/profile'>个人中心</Link></li>
+            <li><NavLink to='/' activeClassName={"active11"} activeStyle={{color:'blue'}} exact>首页</NavLink></li>
+            <li><NavLink to='/user' activeClassName={"active11"} activeStyle={{color:'blue'}}>用户</NavLink></li>
+            <li><NavLink to='/profile' activeClassName={"active11"} activeStyle={{color:'blue'}}>个人中心</NavLink></li>
         </ul>
         <Switch>
             <Route path="/" component={Home} exact/>
