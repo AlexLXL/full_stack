2dcacaf9b8143b5510a6099feb72e3337f6f9fd8
alexLang <l738367@tom.com>
2021-11-29 15:04:01 +0800

    feat(第六章 实现React): 54 路由Link 实现 (就是a标签, 点击的时候加路由跳转操作)

--- 第六章 实现React/100.testReactRouter/src/react-router-dom/index.js
+++ 第六章 实现React/100.testReactRouter/src/react-router-dom/index.js
@@ -1,3 +1,4 @@
 export * from '../react-router'; // 跨平台
 export {default as HashRouter} from './HashRouter';
 export {default as BrowserRouter} from './BrowserRouter';
+export {default as Link} from './Link';
