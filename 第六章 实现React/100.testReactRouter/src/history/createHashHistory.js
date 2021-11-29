c3c6d79779b3c9daf878cdabfcc36530ffb4fc44
alexLang <l738367@tom.com>
2021-11-29 13:19:47 +0800

    feat(第六章 实现React): 51. 实现HashRouter

---
+++ 第六章 实现React/100.testReactRouter/src/history/createHashHistory.js
@@ -0,0 +1,80 @@
+/**
+ * 基于hash
+ * @returns {{goBack: *, go: *, action: string, location: {}, push: *, listen: *}}
+ */
+function createHashHistory() {
+    let stack = [];//类似于历史栈 里面存放都是路径
+    let index = -1;//栈的指针，默认是-1
+    let action = 'POP';//动作
+    let state;//最新的状态
+    let listeners = [];//监听函数的数组
+
+    function go(n) {
+        action = 'POP';
+        index += n;//更改栈顶的指针
+        let nextLocation = stack[index];//取出指定索引对应的路径对象
+        state = nextLocation.state;//取出此location对应的状态
+        window.location.hash = nextLocation.pathname;//修改hash值 ，从而修改当前的路径
+    }
+
+    function goBack() {
+        go(-1);
+    }
+
+    function goForward() {
+        go(1);
+    }
+
+    function push(pathname, nextState) {
+        action = 'PUSH';
+        if (typeof pathname === 'object') {
+            state = pathname.state;
+            pathname = pathname.pathname
+        } else {
+            state = nextState;
+        }
+        window.location.hash = pathname;
+    }
+
+    function listen(listener) {
+        listeners.push(listener);
+        return () => {
+            listeners = listeners.filter(item => item !== listener);
+        }
+    }
+
+    let hashChangeHandler = () => {
+        let pathname = window.location.hash.slice(1);//取出最新的hash值对应的路径  #/user
+        Object.assign(history, {action, location: {pathname, state}});
+        if (action === 'PUSH') {//说明是调用push方法，需要往历史栈中添加新的条目
+            stack[++index] = history.location;
+        }
+        listeners.forEach(listener => listener(history.location));
+    }
+
+    //当hash发生变化的话，会执行回调
+    window.addEventListener('hashchange', hashChangeHandler);
+
+    const history = {
+        action: 'POP',
+        go,
+        goBack,
+        goForward,
+        push,
+        listen,
+        location: {pathname: '/', state: undefined}
+    }
+
+    //如果初始的情况下，如果hash是有值的
+    if (window.location.hash) {
+        action = 'PUSH';
+        hashChangeHandler();
+    } else {
+        action = 'PUSH';
+        window.location.hash = '/';
+    }
+
+    return history;
+}
+
+export default createHashHistory
--- 第六章 实现React/100.testReactRouter/src/react-router-dom/HashRouter.js
+++ 第六章 实现React/100.testReactRouter/src/react-router-dom/HashRouter.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Router} from '../react-router';
-import {createHashHistory} from 'history';
+import {createHashHistory} from '../history';
 
 class HashRouter extends React.Component {
     history = createHashHistory()//HashRouter的history实例属性会指向用hash实现的历史对象
