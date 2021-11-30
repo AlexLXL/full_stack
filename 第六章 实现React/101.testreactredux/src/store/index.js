9865bb32b6f10e34cec71c87d45f4a0110429bf5
alexLang <l738367@tom.com>
2021-11-30 14:11:42 +0800

    feat(第六章 实现React): 69.redux中间件定义: 就是改写store.dispatch

--- 第六章 实现React/101.testreactredux/src/store/index.js
+++ 第六章 实现React/101.testreactredux/src/store/index.js
@@ -3,4 +3,26 @@ import rootReducer from "./reducers";
 
 let store = createStore(rootReducer)
 
+/**
+ * redux中间件
+ * 实例1: 打印更新前后值
+ * 实例2: 发请求
+ */
+/*let oldDispatch = store.dispatch
+store.dispatch = function (action) {
+    console.log(`更新前:`, store.getState())
+    oldDispatch(action)
+    console.log(`更新后:`, store.getState())
+}*/
+/*let oldDispatch = store.dispatch
+store.dispatch = function (action) {
+    fetch('/test.json').then(res => {   // 只能访问public上的, 其他路径都会重定向
+        return res.json()
+    }).then(res => {
+        console.log(res)
+        oldDispatch(action)
+    })
+}*/
+
+
 export default store
