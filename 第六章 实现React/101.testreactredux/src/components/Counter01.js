1c193a6f132ac85b51aad6288859bd13b784dac8
alexLang <l738367@tom.com>
2021-11-30 10:53:45 +0800

    feat(第六章 实现React): 66.react-redux基础使用2

--- 第六章 实现React/101.testreactredux/src/components/Counter01.js
+++ 第六章 实现React/101.testreactredux/src/components/Counter01.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import {connect} from "react-redux";
 import counter01Actions from "../store/action/counter01";
+import * as actionTypes from "../store/action-types";
 
 class Counter01 extends Component {
     render() {
@@ -19,10 +20,21 @@ class Counter01 extends Component {
 // 把仓库状态 映射为 组件的属性对象
 let mapStateToProps = state => state.counter01R
 // 把仓库动作 映射为 组件的属性对象
-
+let mapDispatchToProps = dispatch => ({
+    add1() {
+        dispatch({type: actionTypes.ADD1})
+    },
+    minus1() {
+        dispatch({type: actionTypes.MINUS1})
+    },
+    changeColor1(color) {
+        dispatch({type: actionTypes.CHANGE_COLOR, data: color})
+    }
+})
 export default connect(
     mapStateToProps,
-    counter01Actions
+    mapDispatchToProps, // 第一种写法, 传递一个写好的函数
+    // counter01Actions // 第二种写法, 传递一个对象, 由内部封装
 )(Counter01)
 // 执行connect的效果如下
 // Counter01.props = {...state.counter1, ...actions}
