142278e4fbb1b58b0fa2b25e67a513a32e93d665
alexLang <l738367@tom.com>
2021-11-30 11:21:09 +0800

    feat(第六章 实现React): 66.react-redux实现Provider

---
+++ 第六章 实现React/101.testreactredux/src/zreact-redux/Provider.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+import ReactReduxContext from './ReactReduxContext'
+
+function Provider(props) {
+    return (
+        <ReactReduxContext.Provider value={{store: props.store}}>
+            {props.children}
+        </ReactReduxContext.Provider>
+    )
+}
+
+export default Provider
---
+++ 第六章 实现React/101.testreactredux/src/zreact-redux/connect.js
@@ -0,0 +1,8 @@
+/**
+ * 写两版: 类组件和函数组件hook
+ */
+function connect() {
+
+}
+
+export default connect
