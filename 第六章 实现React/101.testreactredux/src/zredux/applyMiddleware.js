681ea4332d54d49494a4a7cd079cbc892089c596
alexLang <l738367@tom.com>
2021-11-30 17:50:57 +0800

    feat(第六章 实现React): 69.redux中间件级联实现(补充原理)

--- 第六章 实现React/101.testreactredux/src/zredux/applyMiddleware.js
+++ 第六章 实现React/101.testreactredux/src/zredux/applyMiddleware.js
@@ -1,4 +1,4 @@
-import {compose} from "redux";
+/*import {compose} from "redux";
 
 function applyMiddleware(...middlewares) {
     return function (createStore) {
@@ -21,4 +21,100 @@ function applyMiddleware(...middlewares) {
     }
 }
 
-export default applyMiddleware;
+export default applyMiddleware;*/
+
+/**
+ * 原理:
+ * 1.compose组合函数
+ * 2.利用原理1,如何链式调用3个中间件
+ */
+
+/**
+ * 1.compose组合函数
+ */
+/*function add1(str) {
+    return str + 1
+}
+
+function add2(str) {
+    return str + 2
+}
+
+function add3(str) {
+    return str + 3
+}*/
+/**
+ * 需要组合成: add1(add2(add3(str)))
+ */
+/*function compose(...funcs) {
+    /!*!// 这种写法的缺陷, add3参数个数被写死了
+    return function (args) {
+        for (let i = funcs.length - 1; i >= 0; i--) {
+            args = funcs[i](args)
+        }
+        return args
+    }*!/
+    if (funcs.length === 0) {
+        return (args) => args
+    }else if (funcs.length === 1) {
+        return funcs[0]
+    }
+    return funcs.reduce((a, b) => (...args) => a(b(...args)))
+}
+let r2 = compose(add1, add2, add3)('hello')
+console.log(r2) // OUTPUT: hello321*/
+
+
+/**
+ * 2.如何链式调用3个中间件
+ */
+/*const getState = () => 0;
+const dispatch = (action) => {
+    console.log('原始的store.dispatch', action);
+}
+debugger
+let store = {getState, dispatch};
+
+function promise(store) {
+    return function promise(next) {//add1
+        return function (action) {//这个才是真正改造后store.dispatch
+            console.log('promise');
+            next(action);
+        }
+    }
+}
+
+function thunk(store) {
+    return function thunk(next) {//add2
+        return function (action) {
+            console.log('thunk');
+            next(action);
+        }
+    }
+}
+
+function logger(store) {
+    return function logger(next) {//add3
+        return function (action) {
+            console.log('logger');
+            next(action);
+        }
+    }
+}
+
+let middlewares = [promise, thunk, logger];
+let chain = middlewares.map(middleware => middleware(store));
+let composedFn = compose(...chain);
+debugger
+let newDispatch = composedFn(store.dispatch);
+newDispatch({type: 'ADD'});
+
+function compose(...funcs) {
+    return funcs.reduce((a, b) => (...args) => a(b(...args)));
+}
+
+//newDispatch
+function composedFn(store_dispatch) {
+    return promise(thunk(logger(store_dispatch)));
+}*/
+
