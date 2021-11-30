3a345c391a05cef51dbec8126c748c8b8f9113f0
alexLang <l738367@tom.com>
2021-11-30 11:00:57 +0800

    feat(第六章 实现React): 66. 函数组件调用react-redux的hook

--- 第六章 实现React/101.testreactredux/src/components/Counter02.js
+++ 第六章 实现React/101.testreactredux/src/components/Counter02.js
@@ -1,7 +1,24 @@
 import React, {Component} from 'react'
-import {connect} from "react-redux";
+import {useSelector, useDispatch} from "react-redux";
 import counter02Actions from "../store/action/counter02";
 
+function Counter02() {
+    let counter02 = useSelector(state => state.counter02R)
+    let dispatch = useDispatch()
+    let {number, color} = counter02
+    return (
+        <div style={{color: color}}>
+            <p>{number}</p>
+            <button onClick={() => dispatch(counter02Actions.add2())}>+</button>
+            <button onClick={() => dispatch(counter02Actions.minus2())}>-</button>
+            <button onClick={() => dispatch(counter02Actions.changeColor2('yellow'))}>改成黄色</button>
+        </div>
+    )
+}
+
+export default Counter02
+
+/*
 class Counter02 extends Component {
     render() {
         let {color, number, add2, minus2, changeColor2} = this.props
@@ -20,4 +37,4 @@ let mapStateToProps = state => state.counter02R
 export default connect(
     mapStateToProps,
     counter02Actions
-)(Counter02)
+)(Counter02)*/
