933097ed8b80bc954894fb0f6701c19e04003f68
alexLang <l738367@tom.com>
2021-12-02 14:12:28 +0800

    feat(第七章 打包_webPack): 19.webpack代码分割和懒加载基础使用2

--- 第七章 打包相关/003.webpack懒加载/src/index.js
+++ 第七章 打包相关/003.webpack懒加载/src/index.js
@@ -1,5 +1,7 @@
 let btn = document.getElementById("play")
 btn.addEventListener('click', function () {
+    // 模块里的import react from 'react'是一个关键字,
+    // 这里的import是一个方法
     import('./video.js').then(function (res) {
         console.log(res.default)
     })
