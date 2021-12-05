0c3f63729e14c1a6762b554e81e82cc2fe43a610
alexLang <l738367@tom.com>
2021-12-05 14:19:31 +0800

    feat(第七章 打包_webPack): 30.修改文件路径

---
+++ 第七章 打包相关/webpack/003.webpack懒加载/dist/src_video_js.main.js
@@ -0,0 +1,18 @@
+"use strict";
+(self["webpackChunkwritecodeready"] = self["webpackChunkwritecodeready"] || []).push([["src_video_js"],{
+
+/***/ "./src/video.js":
+/*!**********************!*\
+  !*** ./src/video.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ('video11');
+
+/***/ })
+
+}]);
