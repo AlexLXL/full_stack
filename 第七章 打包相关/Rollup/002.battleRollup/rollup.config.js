e3842698c6711d0c2777d94cba24657b76f39c65
alexLang <l738367@tom.com>
2021-12-06 18:11:58 +0800

    feat(第七章 打包_rollup):  1.开始 2.babel

---
+++ 第七章 打包相关/Rollup/002.battleRollup/rollup.config.js
@@ -0,0 +1,15 @@
+import babel from 'rollup-plugin-babel';
+
+export default {
+    input: 'src/main.js',
+    output: {
+        file: 'dist/bundle.cjs.js',//输出文件的路径和名称
+        format: 'cjs',//五种输出格式：amd/es6/iife/umd/cjs
+        name: 'bundleName'//当format为iife和umd时必须提供，将作为全局变量挂在window下
+    },
+    plugins: [
+        babel({
+            exclude: "node_modules/**"
+        })
+    ]
+}
---
+++ 第七章 打包相关/Rollup/002.battleRollup/src/main.js
@@ -0,0 +1,5 @@
+let sum = (a,b)=>{
+    return a+b;
+}
+let result = sum(1,2);
+console.log(result);
