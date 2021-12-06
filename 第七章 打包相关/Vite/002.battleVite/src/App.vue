54580e46b5d7967930c2fbaaae46d27e9c229bd6
alexLang <l738367@tom.com>
2021-12-06 14:46:35 +0800

    feat(第七章 打包_Vite): 4.加入别名

--- 第七章 打包相关/Vite/002.battleVite/src/App.vue
+++ 第七章 打包相关/Vite/002.battleVite/src/App.vue
@@ -1,20 +1,20 @@
 <template>
-    <img src="./assets/1.jpg"/>
+    <img src="@/assets/1.jpg"/>
     <img  :src="imgUrl" />
     <div class="bgWrap"></div>
     <HelloWorld msg="Vue3 + Vite测试"/>
 </template>
 
 <script setup>
-    import imgUrl from './assets/2.jpg'
-    import HelloWorld from './components/HelloWorld.vue'
+    import imgUrl from '@/assets/2.jpg'
+    import HelloWorld from '@/components/HelloWorld.vue'
 </script>
 
 <style scoped>
     .bgWrap {
         width: 200px;
         height: 200px;
-        background-image: url("./assets/3.jpg");
+        background-image: url("@/assets/3.jpg");
         background-repeat: no-repeat;
         background-size: contain;
     }
--- 第七章 打包相关/Vite/002.battleVite/vite.config.js
+++ 第七章 打包相关/Vite/002.battleVite/vite.config.js
@@ -1,7 +1,14 @@
+import {resolve} from 'path'
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue' // 不限于vue, react也可用该插件转换
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()]
+    plugins: [vue()],
+
+    resolve: {
+        alias: {
+            '@': resolve('src')
+        }
+    },
 })
