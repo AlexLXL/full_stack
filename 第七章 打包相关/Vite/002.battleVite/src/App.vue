15b68937ac842ccf1405c6e96621cdf61b2797d3
alexLang <l738367@tom.com>
2021-12-06 14:32:22 +0800

    feat(第七章 打包_Vite): 2.配置开发环境

---
+++ 第七章 打包相关/Vite/002.battleVite/src/App.vue
@@ -0,0 +1,8 @@
+<template>
+    <!--<img src="./assets/ico.jpg"/>-->
+    <HelloWorld msg="Vue3 + Vite测试"/>
+</template>
+
+<script setup>
+    import HelloWorld from './components/HelloWorld.vue'
+</script>
---
+++ 第七章 打包相关/Vite/002.battleVite/vite.config.js
@@ -0,0 +1,7 @@
+import {defineConfig} from 'vite'
+import vue from '@vitejs/plugin-vue' // 不限于vue, react也可用该插件转换
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [vue()]
+})
