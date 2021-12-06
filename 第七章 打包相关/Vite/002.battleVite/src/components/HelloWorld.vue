40392accd6ba68d039379122438a848968ff3906
alexLang <l738367@tom.com>
2021-12-06 15:11:28 +0800

    feat(第七章 打包_Vite): 5.样式处理

--- 第七章 打包相关/Vite/002.battleVite/src/components/HelloWorld.vue
+++ 第七章 打包相关/Vite/002.battleVite/src/components/HelloWorld.vue
@@ -1,7 +1,10 @@
 <template>
     <h1>{{ msg }}</h1>
+    <p class="less">less</p>
+    <p class="sass">sass</p>
+    <div class="postcss"></div>
 </template>
-<script>
+<script scoped>
     export default {
         name: 'HelloWorld',
         props: {
@@ -9,3 +12,23 @@
         },
     }
 </script>
+<style scoped lang="less">
+    @color: red;
+    .less {
+        color: @color;
+    }
+</style>
+<style scoped lang="scss">
+    $color: yellow;
+    .sass {
+        color: $color;
+    }
+</style>
+<style scoped>
+    .postcss {
+        width: 100px;
+        height: 300px;
+        background-color: pink;
+        transform: rotate(45deg);
+    }
+</style>
