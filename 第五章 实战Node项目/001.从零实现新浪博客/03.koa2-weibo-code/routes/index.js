54a643fe9f640fd9bd474af17910a81aa1bd8270
alexLang <l738367@tom.com>
2022-01-13 11:45:45 +0800

    feat(第五章 实战Node项目->从零实现新浪微博): 添加环境变量和项目备注

--- 第五章 实战Node项目/001.从零实现新浪博客/03.koa2-weibo-code/routes/index.js
+++ 第五章 实战Node项目/001.从零实现新浪博客/03.koa2-weibo-code/routes/index.js
@@ -1,16 +1,19 @@
 const router = require('koa-router')()
 
 router.get('/', async (ctx, next) => {
+  // 返回页面
   await ctx.render('index', {
     title: 'Hello Koa 2!'
   })
 })
 
 router.get('/string', async (ctx, next) => {
+  // 返回字符串
   ctx.body = 'koa2 string'
 })
 
 router.get('/json', async (ctx, next) => {
+  // 返回json
   ctx.body = {
     title: 'koa2 json'
   }
