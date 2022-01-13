4f01bc90c4b53e2cb5c08d5251240426fd61f8f4
alexLang <l738367@tom.com>
2022-01-13 17:57:40 +0800

    feat(第五章 实战Node项目->从零实现新浪微博): 2.6 mysql的安装、连接、基础使用

---
+++ 第五章 实战Node项目/001.从零实现新浪博客/03.test/index.js
@@ -0,0 +1,25 @@
+const mysql = require('mysql2')
+
+const con = mysql.createConnection({
+    host: 'localhost',
+    port: '3306',
+    user: 'root',
+    password: 'root',
+    database: 'myblog'
+})
+
+con.connect()
+
+const sql = 'select * from users'
+// const sql = `update users set realname = '李四2' where username = 'lisi'`
+// const sql = `insert into users (username, \`password\`, realname) values ('xiaoming', '123', '小明');`
+
+con.query(sql, (err, result) => {
+    if (err) {
+        console.log(err)
+        return
+    }
+    console.log(result)
+})
+
+con.end()
