bffc8707c9bbd4b9b99d89ef220fa573017ad91e
alexLang <l738367@tom.com>
2022-01-18 11:57:39 +0800

    feat(第五章 实战Node项目->从零实现新浪博客->用户管理->登录/注册页): 获取redis的session, 看是否登录过

--- 第五章 实战Node项目/001.从零实现新浪博客/03.koa2-weibo-code/src/routes/view/user.js
+++ 第五章 实战Node项目/001.从零实现新浪博客/03.koa2-weibo-code/src/routes/view/user.js
@@ -5,12 +5,32 @@
 
 const router = require('koa-router')()
 
+/**
+ * 获取登录信息
+ * @param ctx ctx
+ */
+function getLoginInfo(ctx) {
+    let data = {
+        isLogin: false // 默认未登录
+    }
+
+    const userInfo = ctx.session.userInfo // 根据cookie去redis里拿session, 赋值到ctx.session
+    if (userInfo) {
+        data = {
+            isLogin: true,
+            userName: userInfo.userName
+        }
+    }
+
+    return data
+}
+
 router.get('/login', async (ctx, next) => {
-    await ctx.render('login', {})
+    await ctx.render('login', getLoginInfo(ctx))
 })
 
 router.get('/register', async (ctx, next) => {
-    await ctx.render('register', {})
+    await ctx.render('register', getLoginInfo(ctx))
 })
 
 module.exports = router
