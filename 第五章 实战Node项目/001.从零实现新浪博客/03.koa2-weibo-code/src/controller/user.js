944af1c8ba4209b7f5a868382bd94188ddf96059
alexLang <l738367@tom.com>
2022-01-18 11:43:32 +0800

    feat(第五章 实战Node项目->从零实现新浪博客->用户管理->登录页): 完成"登录"接口,session存储在redis

--- 第五章 实战Node项目/001.从零实现新浪博客/03.koa2-weibo-code/src/controller/user.js
+++ 第五章 实战Node项目/001.从零实现新浪博客/03.koa2-weibo-code/src/controller/user.js
@@ -7,7 +7,8 @@ const {SuccessModel, ErrorModel} = require('../model/ResModel')
 const {
     registerUserNameNotExistInfo,
     registerUserNameExistInfo,
-    registerFailInfo
+    registerFailInfo,
+    loginFailInfo
 } = require('../model/ErrorInfo')
 const {getUserInfo, createUser} = require('../services/user')
 const doCrypto = require('../utils/cryp')
@@ -55,7 +56,30 @@ async function register({ userName, password, gender }) {
     }
 }
 
+/**
+ * 登录
+ * @param ctx koa2 ctx
+ * @param userName 用户名
+ * @param password 密码
+ */
+async function login(ctx, userName, password) {
+    // 业务逻辑处理
+    const userInfo = await getUserInfo(userName, doCrypto(password))
+    if (!userInfo) {
+        // 登录失败
+        return new ErrorModel(loginFailInfo)
+    }
+    // 登录成功
+    if (!ctx.session.userInfo) {
+        ctx.session.userInfo = userInfo
+    }
+
+    // 统一返回格式
+    return new SuccessModel()
+}
+
 module.exports = {
     isExist,
-    register
+    register,
+    login
 }
--- 第五章 实战Node项目/001.从零实现新浪博客/03.koa2-weibo-code/src/routes/api/user.js
+++ 第五章 实战Node项目/001.从零实现新浪博客/03.koa2-weibo-code/src/routes/api/user.js
@@ -4,12 +4,17 @@
  */
 
 const router = require('koa-router')();
-const {isExist, register} = require('../../controller/user')
+const {
+    isExist,
+    register,
+    login
+} = require('../../controller/user')
 const {genValidator} = require('../../middlewares/validator')
 const userValidate = require('../../validator/user')
 
 router.prefix('/api/user/')
 
+// 注册
 router.post('/register', genValidator(userValidate), async (ctx, next) => {
     // 接收参数
     const { userName, password, gender } = ctx.request.body
@@ -17,11 +22,18 @@ router.post('/register', genValidator(userValidate), async (ctx, next) => {
     ctx.body = await register({ userName, password, gender })
 })
 
+// 用户名是否存在
 router.post('/isExist', async (ctx, next) => {
     // 接收参数
     const { userName } = ctx.request.body
     // 返回结果
     ctx.body = await isExist(userName)
+})
+
+// 登录
+router.post('/login', async (ctx, next) => {
+    const { userName, password } = ctx.request.body
+    ctx.body = await login(ctx, userName, password)
 })
 
 module.exports = router
