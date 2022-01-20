47443fca5154010b7a2ef4ee54913c429af549b0
alexLang <l738367@tom.com>
2022-01-20 11:13:04 +0800

    feat(第五章 实战Node项目->从零实现新浪博客->博客): 完成"微博广场"ejs页面、页面路由、API

---
+++ 第五章 实战Node项目/001.从零实现新浪博客/03.koa2-weibo-code/src/routes/api/blog-square.js
@@ -0,0 +1,25 @@
+/**
+ * @description 广场 API 路由
+ * @author 学浪
+ */
+
+const router = require('koa-router')()
+const { loginCheck } = require('../../middlewares/loginChecks')
+const { getSquareBlogList } = require('../../controller/blog-square')
+const { getBlogListStr } = require('../../utils/blog')
+
+router.prefix('/api/square')
+
+// 加载更多
+router.get('/loadMore/:pageIndex', loginCheck, async (ctx, next) => {
+    let { pageIndex } = ctx.params
+    pageIndex = parseInt(pageIndex)  // 转换 number 类型
+    const result = await getSquareBlogList(pageIndex)
+
+    // 渲染模板
+    result.data.blogListTpl = getBlogListStr(result.data.blogList)
+
+    ctx.body = result
+})
+
+module.exports = router
--- 第五章 实战Node项目/001.从零实现新浪博客/03.koa2-weibo-code/src/routes/view/blog.js
+++ 第五章 实战Node项目/001.从零实现新浪博客/03.koa2-weibo-code/src/routes/view/blog.js
@@ -7,6 +7,7 @@ const router = require('koa-router')()
 const {loginRedirect} = require('../../middlewares/loginChecks')
 const {getProfileBlogList} = require('../../controller/blog-profile')
 const { isExist } = require('../../controller/user')
+const { getSquareBlogList } = require('../../controller/blog-square')
 
 
 router.get('/', loginRedirect, async (ctx, next) => {
@@ -75,6 +76,23 @@ router.get('/profile/:userName', loginRedirect, async (ctx, next) => {
             },
             amIFollowed: false,
             atCount: 0
+        }
+    })
+})
+
+// 广场
+router.get('/square', loginRedirect, async (ctx, next) => {
+    // 获取微博数据，第一页
+    const result = await getSquareBlogList(0)
+    const { isEmpty, blogList, pageSize, pageIndex, count } = result.data || {}
+
+    await ctx.render('square', {
+        blogData: {
+            isEmpty,
+            blogList,
+            pageSize,
+            pageIndex,
+            count
         }
     })
 })
