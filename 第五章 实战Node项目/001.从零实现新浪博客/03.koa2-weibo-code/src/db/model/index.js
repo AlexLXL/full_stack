045b7494c0c506c2faf9e3e3c0880181f868c356
alexLang <l738367@tom.com>
2022-01-20 18:08:07 +0800

    feat(第五章 实战Node项目->从零实现新浪博客->博客): 完成"首页-模型"模型修改

--- 第五章 实战Node项目/001.从零实现新浪博客/03.koa2-weibo-code/src/db/model/index.js
+++ 第五章 实战Node项目/001.从零实现新浪博客/03.koa2-weibo-code/src/db/model/index.js
@@ -13,6 +13,17 @@ User.hasMany(UserRelation, {
     foreignKey: 'userId'
 })
 
+/**
+ * 由于上面占用了userId外键, 所以这个在数据库并不会有表现
+ * 而加这行代码是sequelize连表查询的时候使用
+ *
+ * 注意: 在数据表里，外键和连表查询是没有任何关系的, 即使不做外键也可以连表查询
+ */
+Blog.belongsTo(UserRelation, {
+    foreignKey: 'userId',
+    targetKey: 'followerId'
+})
+
 module.exports = {
     User,
     Blog,
