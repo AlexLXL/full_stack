d8cc3c66f3297e3696faf67fc1ab32fb3e3c62b9
alexLang <l738367@tom.com>
2022-01-21 13:59:30 +0800

    feat(第五章 实战Node项目->从零实现新浪博客->博客): 完成"首页-at提到我的"模型

--- 第五章 实战Node项目/001.从零实现新浪博客/03.koa2-weibo-code/src/db/model/index.js
+++ 第五章 实战Node项目/001.从零实现新浪博客/03.koa2-weibo-code/src/db/model/index.js
@@ -1,6 +1,7 @@
 const User = require('./User')
 const Blog = require('./Blog')
 const UserRelation = require('./UserRelation')
+const AtRelation = require('./AtRelation')
 
 Blog.belongsTo(User, {
     foreignKey: 'userId'
@@ -24,8 +25,13 @@ Blog.belongsTo(UserRelation, {
     targetKey: 'followerId'
 })
 
+Blog.hasMany(AtRelation, {
+    foreignKey: 'blogId',
+})
+
 module.exports = {
     User,
     Blog,
-    UserRelation
+    UserRelation,
+    AtRelation
 }
