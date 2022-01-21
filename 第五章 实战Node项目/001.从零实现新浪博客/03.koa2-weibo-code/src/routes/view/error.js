b0144900bf2ee5ca94155c3b7775f54c4414c512
alexLang <l738367@tom.com>
2022-01-21 16:40:36 +0800

    feat(第五章 实战Node项目->从零实现新浪博客->博客): 线上环境-pm2部署使用

--- 第五章 实战Node项目/001.从零实现新浪博客/03.koa2-weibo-code/src/routes/view/error.js
+++ 第五章 实战Node项目/001.从零实现新浪博客/03.koa2-weibo-code/src/routes/view/error.js
@@ -9,6 +9,15 @@ router.get('/error', async (ctx, next) => {
     await ctx.render('error')
 })
 
+// 故意制造一个错误
+router.get('/get-an-error', async (ctx, next) => {
+    console.error('发生错误2')
+    throw new Error('发生错误1')
+    ctx.body = {
+        msg: 'xxx'
+    }
+})
+
 router.get('*', async (ctx, next) => {
     await ctx.render('404')
 })
