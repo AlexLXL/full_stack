4e48ad48af1d20aea51c31e2748bf911c2f0b041
alexLang <l738367@tom.com>
2022-01-21 10:45:16 +0800

    feat(第五章 实战Node项目->从零实现新浪博客->博客): 完成"首页-加载更多"API和单元测试

--- 第五章 实战Node项目/001.从零实现新浪博客/03.koa2-weibo-code/src/routes/api/blog-home.js
+++ 第五章 实战Node项目/001.从零实现新浪博客/03.koa2-weibo-code/src/routes/api/blog-home.js
@@ -8,6 +8,8 @@ const {loginCheck} = require('../../middlewares/loginChecks')
 const {create} = require('../../controller/blog-home')
 const blogValidate = require('../../validator/blog')
 const {genValidator} = require('../../middlewares/validator')
+const { getHomeBlogList } = require('../../controller/blog-home')
+const { getBlogListStr } = require('../../utils/blog')
 
 router.prefix('/api/blog')
 
@@ -18,6 +20,18 @@ router.post('/create', loginCheck, genValidator(blogValidate), async (ctx, next)
     const {id: userId} = ctx.session.userInfo
     // 返回结果
     ctx.body = await create({userId, content, image})
+})
+
+// 加载更多
+router.get('/loadMore/:pageIndex', loginCheck, async (ctx, next) => {
+    let { pageIndex } = ctx.params
+    pageIndex = parseInt(pageIndex)  // 转换 number 类型
+    const { id: userId } = ctx.session.userInfo
+    const result = await getHomeBlogList(userId, pageIndex)
+    // 渲染模板
+    result.data.blogListTpl = getBlogListStr(result.data.blogList)
+
+    ctx.body = result
 })
 
 module.exports = router
--- 第五章 实战Node项目/001.从零实现新浪博客/03.koa2-weibo-code/test/blog/home.test.js
+++ 第五章 实战Node项目/001.从零实现新浪博客/03.koa2-weibo-code/test/blog/home.test.js
@@ -3,6 +3,21 @@ const {Z_COOKIE} = require('../testUserInfo')
 
 let BLOG_ID = ''
 
+// 加载更多
+test('首页，加载第一页数据，应该成功', async () => {
+    const res = await server
+        .get(`/api/blog/loadMore/0`)
+        .set('cookie', Z_COOKIE)
+    expect(res.body.errno).toBe(0)
+
+    const data = res.body.data
+    expect(data).toHaveProperty('isEmpty')
+    expect(data).toHaveProperty('blogList')
+    expect(data).toHaveProperty('pageSize')
+    expect(data).toHaveProperty('pageIndex')
+    expect(data).toHaveProperty('count')
+})
+
 // 创建微博
 test('创建微博, 应该成功', async () => {
     // 定义测试内容
@@ -24,3 +39,4 @@ test('创建微博, 应该成功', async () => {
     // 记录微博 id
     BLOG_ID = res.body.data.id
 })
+
